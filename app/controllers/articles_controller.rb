5d77eb4acbe03b63e9a6da8009db0b0f54d57f0b
kklaiber <kylesklaiber@gmail.com>
2020-01-24 22:11:22 -0800

    Fix create, add flash and err messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+    @article = Article.find(params[:id])
   end
 
   def create
     #render plain: params[:article].inspect -----> this shows the hash from the params hash
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+      flash[:notice] = "Your article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   #This defines the article params method that is used in the create action. This 'white lists' the values of article. The params.require takes in the :article KEY and .permit the :title and :description VALUES.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  # validates :title presence: true, length: {minimum: 1, maximum: 80}
-  # validates :description presence: true, length: {minimum: 1, maximum: 10000 }
+  validates :title, presence: true, length: {minimum: 1, maximum: 80}
+  validates :description, presence: true, length: {minimum: 1, maximum: 10000 }
 end
