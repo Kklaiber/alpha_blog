5e49e3b6f6645a6cb2b3b7b7a71e2b2d743e361a
kklaiber <kylesklaiber@gmail.com>
2020-02-08 21:39:02 -0800

    added edit, update, show, index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,26 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-    @name = params[:name]
-  end
 
   def index
-    @name = params[:name]
+    @articles = Article.all
   end
 
-  def show
+  def new
+    @article = Article.new
+  end
+
+  def edit
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Your article was successfully updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -22,6 +33,10 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   #This defines the article params method that is used in the create action. This 'white lists' the values of article. The params.require takes in the :article KEY and .permit the :title and :description VALUES.
